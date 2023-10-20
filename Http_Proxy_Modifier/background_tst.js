54f7e7a0cdb88d76275f7cd36fa7b06b2a8e0836
Adrian Mirabal <adrian.mirabal07@gmail.com>
2023-10-20 10:37:11 -0400

    Initial commit

---
+++ Http_Proxy_Modifier/background_tst.js
@@ -0,0 +1,34 @@
+/**Replace YOUR_PROXY_HOST, YOUR_PROXY_PORT, YOUR_USERNAME, YOUR_PASSWORD, 
+ * and YOUR_NEW_REFERER_VALUE with the appropriate values you want to hard code. */
+
+browser.proxy.onRequest.addListener(
+    request => {
+      // Modify proxy settings for HTTP requests
+      if (request.url.startsWith("http")) {
+        return {
+          type: "http",
+          host: "localhost",
+          port: 8888,
+        };
+      }
+      return null;
+    },
+    { urls: ["<all_urls>"] }
+  );
+  
+  // Modify HTTP headers
+  browser.webRequest.onBeforeSendHeaders.addListener(
+    details => {
+      for (let i = 0; i < details.requestHeaders.length; i++) {
+        if (details.requestHeaders[i].name.toLowerCase() === "referer") {
+          // Modify the Referer header value
+          details.requestHeaders[i].value = "YOUR_NEW_REFERER_VALUE";
+          break;
+        }
+      }
+      return { requestHeaders: details.requestHeaders };
+    },
+    { urls: ["<all_urls>"] },
+    ["blocking", "requestHeaders"]
+  );
+  
